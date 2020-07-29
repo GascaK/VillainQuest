80e92f37bd319d03bea1e7ad3829988c988f9e97
Kevin Gasca <gasca.kevin.a@gmail.com>
2020-07-28 21:14:23 -0700

    [0.08] Player/Hero classes
    * Created Player class with health.
    * Created Hero class, fixed collision on player.

--- flaskr/static/scripts/VQuest.js
+++ flaskr/static/scripts/VQuest.js
@@ -5,11 +5,6 @@ class VQuest extends Phaser.Scene {
 
     preload()
     {
-        // Load imgs
-        /*this.load.spritesheet('player', 'dude.png',
-            { frameWidth: 66, frameHeight: 60 }
-        );*/
-
         // Static is somehow relative to flaskr location. Need to verify
         // this will be true in production.
         this.load.image('background', '/static/scripts/assets/floor.jpg');
@@ -28,20 +23,33 @@ class VQuest extends Phaser.Scene {
 
         // Players and reticle
         var background = this.add.image(800, 600, 'background');
-        player = this.physics.add.image(800, 600, 'villain');
         reticle = this.physics.add.sprite(800, 700, 'crosshair')
 
         background.setOrigin(0.5, 0.5).setDisplaySize(1600, 1200);
-        player.setOrigin(0.5, 0.5).setDisplaySize(132,120).setCollideWorldBounds(true).setDrag(500,500);
         reticle.setOrigin(0.5, 0.5).setDisplaySize(25, 25).setCollideWorldBounds(true);
         
         // Camera zoom
         this.cameras.main.zoom = 0.5;
 
+        // Player
+        player = new Player({
+            scene: this,
+            name: 'player',
+            health: 100,
+            x: 800, y: 600,
+            texture: 'villain'
+        });
+
         // Bad Guys
-        var hero = new Hero({scene: this, x: 400, y: 16});
-        this.physics.add.collider(player, hero, this.hitPlayer, null, this);
-        //hero.body.setVelocity(10, 10);
+        this.hero = new Hero({
+            scene: this, 
+            name: 'hero',
+            health: 100, 
+            x: 400, y: 200, 
+            texture: 'hero'
+        });
+
+        //this.physics.add.collider(player, this.hero, this.playerHit, null, this);
 
         // Movement Keys
         moveKeys = this.input.keyboard.addKeys({
@@ -118,12 +126,6 @@ class VQuest extends Phaser.Scene {
         }, this);
 
     }
-    
-    hitPlayer()
-    {
-        this.physics.pause();
-        this.gameOver = true;
-    }
 
     constrainVelocity(sprite, maxVelocity)
     {
@@ -175,8 +177,41 @@ class VQuest extends Phaser.Scene {
         }
 
     }
-
+    
     update(time, delta)
+    {
+        // Badguy Update
+        this.hero.update(time, delta);
+
+        // Player Update
+        player.update(time, delta);
+
+        console.log(player.health);
+
+        if (player.health < 0)
+        {
+            player.health = 100;
+            this.scene.start('MainMenu');
+        }
+    }
+
+}
+
+class Player extends Phaser.Physics.Arcade.Sprite {
+    constructor(data)
+    {
+        super(data.scene, data.x, data.y, data.texture, 'Player');
+        this.data = data;
+        this.health = data.health;
+
+        this.data.scene.physics.world.enable(this);
+        this.setOrigin(0.5, 0.5).setDisplaySize(132, 120).setDrag(500, 500);
+        this.body.collideWorldBounds = true;
+
+        data.scene.add.existing(this);
+    }
+
+    update()
     {
         // Rotates player to face towards reticle
         player.rotation = Phaser.Math.Angle.Between(player.x, player.y, reticle.x, reticle.y);
@@ -184,38 +219,111 @@ class VQuest extends Phaser.Scene {
         // Camera position
         var avgX = ((player.x + reticle.x)/2) - 400;
         var avgY = ((player.y + reticle.y)/2) - 300;
-        this.cameras.main.scrollX = avgX;
-        this.cameras.main.scrollY = avgY;
+        this.data.scene.cameras.main.scrollX = avgX;
+        this.data.scene.cameras.main.scrollY = avgY;
 
         // Reticle moves with player
         reticle.body.velocity.x = player.body.velocity.x;
         reticle.body.velocity.y = player.body.velocity.y;
 
         this.constrainVelocity(player, 500);
-
         this.constrainReticle(reticle, 550);
+    }
 
-        if (this.gameOver === true)
+    constrainVelocity(sprite, maxVelocity)
+    {
+        if(!sprite || !sprite.body)
+            return;
+
+        // Gets current velocity of sprite
+        var angle, currVelocitySqr, vx, vy;
+        vx = sprite.body.velocity.x;
+        vy = sprite.body.velocity.y;
+        currVelocitySqr = vx * vx + vy * vy;
+
+        // Math
+        if(currVelocitySqr > maxVelocity * maxVelocity)
         {
-            this.gameOver = false;
-            this.scene.start('MainMenu');
+            angle = Math.atan2(vy, vx);
+            vx = Math.cos(angle) * maxVelocity;
+            vy = Math.sin(angle) * maxVelocity;
+            sprite.body.velocity.x = vx;
+            sprite.body.velocity.y = vy;
+        }
+    }
+
+    constrainReticle(reticle, radius)
+    {
+        var distX = reticle.x-player.x; // x dist player and ret
+        var distY = reticle.y-player.y; // y dist player and ret
+
+        // prevent reticle from going out of bounds
+        if(distX > 800)
+            reticle.x = player.x+800;
+        else if(distX < -800)
+            reticle.x = player.x-800;
+
+        if(distY > 600)
+            reticle.y = player.y+600;
+        else if(distY < -600)
+            reticle.y = player.y-600;
+
+        // prevent move away from player
+        var distBetween = Phaser.Math.Distance.Between(player.x, player.y, reticle.x, reticle.y);
+        if(distBetween > radius)
+        {
+            // Place reticle on perimeter of circle on line intersecting player and reticle
+            var scale = distBetween/radius;
+
+            reticle.x = player.x + (reticle.x - player.x) / scale;
+            reticle.y = player.y + (reticle.y - player.y) / scale;
         }
     }
 }
 
-class Hero extends Phaser.GameObjects.Sprite{
+class Hero extends Phaser.Physics.Arcade.Sprite {
     constructor(data)
     {
-        super(data.scene, data.x, data.y, 'Hero');
+        super(data.scene, data.x, data.y, data.texture, 'Hero');
+        this.data = data;
+
+        this.xVel = 0;
+        this.yVel = 0;
+
+        data.scene.physics.world.enable(this);
+        this.body.collideWorldBounds = true;
+
+        this.speed = 2;
         data.scene.add.existing(this);
-        this.physics.add.collider(this, player);
-    }
-    
-    click()
-    {
-        console.log('Clicked');
+
+        data.scene.physics.add.collider(this, player, this.playerHit, null, this);
     }
 
+    playerHit()
+    {
+        console.log('hit');
+        player.health -= 10;
+    }
+
+    update(time, delta)
+    {
+        this.rotation = Phaser.Math.Angle.Between(this.x, this.y, player.x, player.y);
+        this.direction = Math.atan( (player.x-this.x) / (player.y-this.y) );
+
+        if(player.y >= this.y)
+        {
+            this.xVel = this.speed*Math.sin(this.direction);
+            this.yVel = this.speed*Math.cos(this.direction);
+        }
+        else
+        {
+            this.xVel = -this.speed*Math.sin(this.direction);
+            this.yVel = -this.speed*Math.cos(this.direction);
+        }
+
+        this.body.setVelocityX(this.xVel * delta);
+        this.body.setVelocityY(this.yVel * delta);
+    }
 }
 
 class MainMenu extends Phaser.Scene {
